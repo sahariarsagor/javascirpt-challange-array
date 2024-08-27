509d37ee7af7e2ca9efc016ef47e8e3052414002
Sahariar Sagor <contactsahariar@gmail.com>
2024-08-27 19:05:11 +0800

    add problem 01

--- challange-01.js
+++ challange-01.js
@@ -1,1 +1,10 @@
-console.log("Sahariar Sagor");
+// Write a JavaScript function to check whether an 'input' is an array or not.
+
+//Solution 
+function isArray(input){
+    return Array.isArray(input);
+}
+
+//testing passing a string and a array
+console.log(isArray("Sahariar Sagor")); //print false 
+console.log(isArray([1, 2, 3])); //print true
